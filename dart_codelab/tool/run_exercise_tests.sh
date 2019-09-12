05deee478ba7de8255b0fb1ca6e815f8ef889d3b
Andrew Brogdon <redbrogdon@google.com>
2019-09-12 10:13:16 -0700

    Reworked pubspec

--- dart_codelab/tool/run_exercise_tests.sh
+++ dart_codelab/tool/run_exercise_tests.sh
@@ -33,6 +33,12 @@ do
 
     cat "../${EXERCISE_NAME}/solution.dart" "../${EXERCISE_NAME}/test.dart" > combined.dart
 
+    # DartPad injexts a method called `_result` into the combined user/test code to give it a
+    # way to report results out it the DartPad UI. This code is injected here so that:
+    #
+    # * The `_result` function is present (without it the code would fail to compile).
+    # * The outer test method (in `exercise_test.dart`) can be used to verify that the combined
+    #   user/test code works as it should by checking the output of this `_result` function.
     echo '
 
     bool lastSuccess;
